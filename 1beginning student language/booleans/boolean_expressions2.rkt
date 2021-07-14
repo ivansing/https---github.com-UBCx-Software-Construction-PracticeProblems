a6af88ac20643ea226b5c6d787b00faf4d5598dc
Ivan <19809417+ivansing@users.noreply.github.com>
2021-07-13 20:14:05 -0500

    Uploades files from January 2021

---
+++ 1beginning student language/booleans/boolean_expressions2.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname boolean_expressions2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(define I1 (rectangle 10 20 "solid" "red"))
+(define I2 (rectangle 20 10 "solid" "blue"))
+
+(if (< (image-width I1)
+       (image-height I1))
+       "tall"
+       "wide")
+
+(and (> (image-height I1) (image-height I2))
+     (< (image-width I1) (image-width I2)))
---
+++ 1beginning student language/functions/questions1-4_stepper.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname questions1-4_stepper) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (foo x) (* x 2))
+
+(foo (+ 4 5))
+
+(foo 9)
+
+(* 9 2)
