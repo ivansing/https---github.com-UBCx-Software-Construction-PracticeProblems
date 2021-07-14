4c347fb6fcf27a89826f5fd64b70f1cc94fe7dac
Ivan <19809417+ivansing@users.noreply.github.com>
2021-07-13 21:36:30 -0500

    Uploaded files Natural
    From UDX course UBC

---
+++ 8Natural/test.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname test) (read-case-sensitive #t) (teachpacks ((lib "abstraction.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "abstraction.rkt" "teachpack" "2htdp")) #f)))
+(add1 0)
+(add1 (add1 0))
+(sub1 2)
+(sub1 (sub1 2))
